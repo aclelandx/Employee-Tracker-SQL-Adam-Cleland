9da6e38a96f2e9a3fb480b86f03c2398c654218d
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-03-28 07:06:50 -0400

    Added comments for the index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // import the application from the local file it is stored in.
 const EmployeeCMS = require(`./applicationClass`)
 
+// create a new instance of the application
 const beginCMS = new EmployeeCMS()
+// invoke the applications starting function to allow the program to begin running.
 beginCMS.welcomeMessage();
