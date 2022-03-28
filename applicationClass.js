88b8b5143ed34a7ee10abe499080f870362896e0
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-03-28 07:35:08 -0400

    Added additional comments to the applicationCalss.js file

--- applicationClass.js
+++ applicationClass.js
@@ -84,17 +84,19 @@ class EmployeeCMS {
             .then(([departments]) => {
                 console.table(departments);
             });
+        // sends to redirect back to the home screen via the backToHome method
         this.backToHome()
     }
-
+    // Queries into the database to see all of the current jobs that are stored inside of it.
     async viewAllJobs() {
         await db.promise().query('SELECT job_title.id, job_title.title, job_title.salary, department.department_name AS "Department Name" FROM job_title JOIN department ON job_title.department_id = department.id ORDER BY job_title.id')
             .then(([positions]) => {
                 console.table(positions);
             })
+        // sends to redirect back to the home screen via the backToHome method
         this.backToHome()
     }
-
+    // queries into the database to see all of the current employees that are stored inside of it. They are joined together via the relationships that have been set up in the schema so all the relevant information can be viewed in one centralized location.
     async viewAllEmployees() {
         await db.promise().query(`
         SELECT employee.id, 
@@ -111,10 +113,12 @@ class EmployeeCMS {
             .then(([employees]) => {
                 console.table(employees);
             })
+        // sends to redirect back to the home screen via the backToHome method
         this.backToHome();
     }
-
+    // allows the user to add a custom department into the database.
     async addDepartment() {
+        // takes in the users input for a new department to be added.
         const newDepartment = await inquirer.prompt([
             {
                 message: `What is the Name of the new Department?`,
@@ -122,12 +126,15 @@ class EmployeeCMS {
                 name: `name`
             }
         ])
+        // Takes the information from the inquirer prompt and queries into the database to insert a new item into the department table
         await db.promise().query(`INSERT INTO department (department_name) VALUES ('${newDepartment.name}');`);
         console.log(`New Department Has Been Added`);
+        // Redirects to the viewAllDepartments method to show the newly updated information
         this.viewAllDepartments();
     }
-
+    // allows the user to add a custom job position into the database.
     async addJobPosition() {
+        // takes in the users information for the name of the position, along with the salary for the position.
         const newJobPosition = await inquirer.prompt([
             {
                 message: `What is the name of the new job position?`,
@@ -140,20 +147,24 @@ class EmployeeCMS {
                 name: `salary`
             },
         ])
+        // since the third parameter for the new job position is asking about which department that it belongs to, The Id and the name of all the departments that are currently located inside of the database are listed out so the user can pick which one that it fits into with ease.
         await db.promise().query(`SELECT * FROM department ORDER BY department.id`).then(([data]) => console.table(data))
+        // grab the users input for which department the new job is located in.
         const departmentOwner = await inquirer.prompt({
             message: `Which Department does this position belong to? (please an id from the list above)`,
             type: `input`,
             name: `name`
         })
-
+        // Takes all of the information provided by the user and queries into the database to insert into the job_title table to add the newly defined custom job.
         await db.promise().query(`INSERT INTO job_title (title, salary, department_id) 
         VALUES ('${newJobPosition.title}', ${newJobPosition.salary}, ${departmentOwner.name});`);
         console.log(`The job position of ${newJobPosition.title} has been added to the database`);
+        // redirects back to the jobs screen so the newly added job can be seen along with the ones that have existed before.
         this.viewAllJobs();
     }
-
+    // allows the user to add a new employee into the roster / team.
     async addEmployee() {
+        // allows the user to input the information  for the first and last name for the new employee. This information is then saved inside of a constant variable called newEmployeeName
         const newEmployeeName = await inquirer.prompt([
             {
                 message: `What is the new Employee's First name?`,
@@ -166,35 +177,38 @@ class EmployeeCMS {
                 name: `lastName`
             }
         ])
+        // because the third parameter for the employee is required to have a job title set for them, all of the current job titles that exist inside of the database are displayed so the user is able to pick one by id with ease.
         await db.promise().query(`SELECT job_title.id, job_title.title FROM job_title`).then(([data]) => {
             console.table(data);
         })
+        // grabs the users selection of which job to assign this employee.
         const newEmployeeJT = await inquirer.prompt({
             message: `Please Select one of the existing Job Title ID numbers for this Employee`,
             type: `input`,
             name: `jobT`
         })
-
+        // because the final parameter for an employee to be added is dependant on the managers ID, all of the current employees id and name are grabbed so the user can pick one with ease (I decided to not make this restricted to only those who's manager title is set to null, because managers can have managers as well.)
         await db.promise().query(`SELECT employee.id, CONCAT(employee.first_name, ' ', employee.last_name) AS 'Managers Name' FROM employee`).then(([data]) => { console.table(data) })
-
+        // grabs the users selection for the ID of which employee is going to be set to the manager for the New employee.
         const isManager = await inquirer.prompt({
             message: `Who is the Manager for the new employee? (Select an ID from the list above)\n *If the New Employee is a manager type NULL*\n`,
             type: `input`,
             name: `manager`
         })
-
+        // Takes all the information provided by the user and queries into the database to insert the new employee into the employee database.
         await db.promise().query(`INSERT INTO employee (first_name, last_name, job_title_id, manager_id)
         VALUES ('${newEmployeeName.firstName}','${newEmployeeName.lastName}',${newEmployeeJT.jobT},${isManager.manager});`)
 
         console.log(`${newEmployeeName.firstName} ${newEmployeeName.lastName} has been added to the database.`)
-
+        // sends to redirect back to the home screen via the backToHome method
         this.backToHome();
 
     }
-
+    // allows the user to change information of an individual employee
     async updateEmployee() {
         // displays the employees information so it can be references more easily
         await db.promise().query(`SELECT employee.id AS 'Employee ID', CONCAT(employee.first_name, ' ', employee.last_name) AS 'Employee Name' FROM employee ORDER BY employee.id`).then(([employees]) => { console.table(employees) })
+        // grabs the users selection of the employee that they wish to edit, along with asking them if they would like to update said employee or delete them from the database
         const changeEmployee = await inquirer.prompt([
             {
                 message: `Which Employee Will you be changing? (Please input the Associated ID number)`,
@@ -212,6 +226,7 @@ class EmployeeCMS {
                 default: `Update Information`
             }
         ])
+        // switch case in order to direct the selection to the proper method according to the users input
         switch (changeEmployee.changeHow) {
             case `Update Information`:
                 this.changeEmployeeInformation(changeEmployee.changeWho)
@@ -223,8 +238,9 @@ class EmployeeCMS {
                 break;
         }
     }
-
+    // allows the user to specify what they would like to change about the selected employee
     async changeEmployeeInformation(targetEmployee) {
+        // grabs the users selection for what they will be changing
         const newEmployeeInformation = await inquirer.prompt([
             {
                 message: `What would you like to change for employee id : ${targetEmployee}?`,
@@ -235,6 +251,7 @@ class EmployeeCMS {
                 ]
             }
         ])
+        // directs the selection to the proper function to be executed
         switch (newEmployeeInformation.changeHow) {
             case `Change Job Position`:
                 await db.promise().query(`SELECT job_title.id, job_title.title FROM job_title`)
